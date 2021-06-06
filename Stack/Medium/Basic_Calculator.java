2c5f53de950d8f3938a11b44667ea82719db3040
jeenalkava <jeenalkava19@gmail.com>
2021-06-06 19:15:43 +0530

    Create Basic_Calculator.java

---
+++ Stack/Medium/Basic_Calculator.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int calculate(String s) {
+
+        if (s == null || s.length() == 0) return 0;
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                }
+                else if (operation == '+') {
+                    stack.push(currentNumber);
+                }
+                else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                }
+                else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
+
+Time : O(n)
+Space : O(m)

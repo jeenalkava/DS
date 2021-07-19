e4edca34a99dc4de17e5a2ab13c0e5c69cd24bbf
jeenalkava <jeenalkava19@gmail.com>
2021-07-19 22:05:30 +0530

    Create Valid_Paranthesis.java

---
+++ Strings/Easy/Valid_Paranthesis.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<Character>();
+        
+        if(s.length()%2!=0)
+            return false;
+        
+        for(char i : s.toCharArray())
+        {
+            if(i=='(' ||i=='{' ||i=='[')
+            
+                stack.push(i);
+            
+            else if(i==')' && !stack.isEmpty() && stack.peek()=='(')
+                stack.pop();
+            
+             else if(i=='}' && !stack.isEmpty() && stack.peek()=='{')
+                stack.pop();
+            
+             else if(i==']' && !stack.isEmpty() && stack.peek()=='[')
+                stack.pop();
+            
+             else
+                 return false;
+            
+        }
+        return stack.isEmpty();
+        
+    }
+}
+
+Time : O(n)
+Space : O(n)

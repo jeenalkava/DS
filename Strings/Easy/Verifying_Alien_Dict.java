3eea3925656d913d54d77a0718bc820a7d337450
jeenalkava <jeenalkava19@gmail.com>
2021-07-29 20:32:32 +0530

    Create Verifying_Alien_Dict.java

---
+++ Strings/Easy/Verifying_Alien_Dict.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int char_map[];
+    public boolean isAlienSorted(String[] words, String order) {
+         char_map=new int[26];
+        for(int i=0;i<order.length();i++)
+        {
+            char_map[order.charAt(i)-'a']=i;
+        }
+        for(int i=1;i<words.length;i++)
+        {
+            if(compare(words[i-1],words[i])>0)
+                return false;
+          
+        }
+          return true;
+        
+        
+    }
+    public int compare(String word1,String word2)
+    {
+        int i=0;
+        int j=0;
+        int char_compare=0;
+        while(i<word1.length() && j<word2.length() && char_compare==0)
+        {
+            char_compare=char_map[word1.charAt(i)-'a']-char_map[word2.charAt(i)-'a'];
+            i++;
+            j++;
+         }
+        if(char_compare==0)
+        {
+            return word1.length()-word2.length();
+                
+            
+        }
+        else
+            return char_compare;
+        
+    }
+}
+
+o(n2)
+o(n)
